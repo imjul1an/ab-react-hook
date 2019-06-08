154115658eecb6efbc76164f06943f851a63f438
julianusti <julian.ustiyanovych@gmail.com>
2019-06-08 22:46:56 +0200

    remove unused utils

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const isThenable = (p: any): boolean => {
-  return (
-    p !== null &&
-    (typeof p === "object" || typeof p === "function") &&
-    typeof p.then === "function"
-  );
-};
