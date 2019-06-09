62c4b385d0398203264cc6d3cebe0b561cf2d8dc
julianusti <julian.ustiyanovych@gmail.com>
2019-06-09 21:15:46 +0200

    feat: useExperimentAsync()

---
+++ src/useExperimentAsync.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { NONE_VARIANT } from "./constants";
+import { ExperimentResultAsync, Variant } from "./interfaces";
+
+export default function useExperimentAsyc(
+  asyncFn: (...args: any[] | []) => Promise<any>,
+  logger: any = console
+): ExperimentResultAsync {
+  const [variant, setVariant] = useState<Variant>({ name: undefined, weight: undefined });
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+        try {
+
+          setLoading(true);
+          const { name, weight } = await asyncFn();
+
+          setLoading(false);
+
+          setVariant({ name, weight });
+        } catch (err) {
+          logger.error(err);
+          setLoading(false);
+          setVariant({ name: NONE_VARIANT, weight: undefined });
+        }
+      })();
+  }, [asyncFn, logger]);
+
+  return { variant, isLoading };
+}
